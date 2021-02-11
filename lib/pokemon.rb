e1f334fb83985edf75c3ddaeb46c595ebdd89f92
Luke Gill <bagrad007@gmail.com>
2021-02-11 16:08:40 -0500

    commit tree 75cb73732775d2a8b48e3b5f6e6020175dbef91b

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
+require "pry"
+
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(id: nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, obj)
+    # binding.pry
+    sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+        SQL
+    obj.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+    SELECT * 
+    FROM pokemon
+    WHERE id = ?
+    SQL
+
+    exe = db.execute(sql, id)[0]
+    self.new(id: exe[0], name: exe[1], type: exe[2], db: db)
+  end
 end
